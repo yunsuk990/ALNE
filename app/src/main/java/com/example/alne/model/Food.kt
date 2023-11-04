3dbbfe00206563c93779f78fa2995ef2597f8bc9
yunsuk990 <yunsuk990@naver.com>
2023-11-04 11:50:11 +0900

    Update

---
+++ app/src/main/java/com/example/alne/model/Food.kt
@@ -0,0 +1,7 @@
+package com.example.alne.model
+
+data class Food(
+    var name: String,
+    var image: String,
+    var expireDate: String,
+)
--- app/src/main/java/com/example/alne/view/HomeFragment.kt
+++ app/src/main/java/com/example/alne/view/HomeFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.alne.R
 import com.example.alne.databinding.FragmentHomeBinding
+import com.example.alne.model.Food
 
 class HomeFragment : Fragment() {
 
@@ -17,6 +19,25 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(layoutInflater)
+
+
+        var textItem: ArrayList<Food> = ArrayList()
+        textItem.add(Food("바나나",R.drawable.food.toString(),"2022.12.21"))
+        textItem.add(Food("오렌지",R.drawable.food.toString(),"2022.12.21"))
+        textItem.add(Food("사과",R.drawable.food.toString(),"2022.12.21"))
+        textItem.add(Food("옥수수",R.drawable.food.toString(),"2022.12.21"))
+        textItem.add(Food("옥수수",R.drawable.food.toString(),"2022.12.21"))
+        textItem.add(Food("옥수수",R.drawable.food.toString(),"2022.12.21"))
+        textItem.add(Food("옥수수",R.drawable.food.toString(),"2022.12.21"))
+        textItem.add(Food("옥수수",R.drawable.food.toString(),"2022.12.21"))
+        textItem.add(Food("옥수수",R.drawable.food.toString(),"2022.12.21"))
+        textItem.add(Food("옥수수",R.drawable.food.toString(),"2022.12.21"))
+        var adapter = ExpireAdapter(textItem)
+        binding.homeItemRv.adapter = adapter
+
+
+
+
         return binding.root
     }
 
