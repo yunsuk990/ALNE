bb0f2a2db1b8c3f883a3b4b15fb380ad07fa9f04
yunsuk990 <yunsuk990@naver.com>
2023-11-17 15:21:59 +0900

    FridgeActivity Update

--- app/src/main/java/com/example/alne/view/FridgeAdapter.kt
+++ app/src/main/java/com/example/alne/view/FridgeAdapter.kt
@@ -3,15 +3,25 @@ package com.example.alne.view
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.alne.R
 import com.example.alne.databinding.ItemFridgeBinding
 import com.example.alne.model.Food
 
 class FridgeAdapter(private val items: ArrayList<Food>): RecyclerView.Adapter<FridgeAdapter.ViewHolder>() {
 
+    interface MyItemClickListener {
+        fun onItemClick(food: Food)
+    }
+    private lateinit var mItemClickListener : MyItemClickListener
+    fun setMyItemClickListener(itemClickListener: MyItemClickListener){
+        mItemClickListener = itemClickListener
+    }
+
     inner class ViewHolder(val binding: ItemFridgeBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(food: Food){
             binding.itemFridgeTitleTv.text = food.name
             binding.itemFridgeExpireTv.text = food.expireDate
+            binding.itemFridgeIv.setImageResource(R.drawable.bibimbap)
         }
     }
 
@@ -22,7 +32,25 @@ class FridgeAdapter(private val items: ArrayList<Food>): RecyclerView.Adapter<Fr
 
     override fun onBindViewHolder(holder: FridgeAdapter.ViewHolder, position: Int) {
         holder.bind(items[position])
+        holder.binding.root.setOnClickListener{
+            mItemClickListener.onItemClick(items[position])
+        }
+
+        holder.binding.itemFridgeDeleteIb.setOnClickListener{
+            deleteFood(position)
+        }
     }
+
+    fun addFood(food: Food){
+        items.add(food)
+        notifyDataSetChanged()
+    }
+
+    fun deleteFood(position: Int){
+        items.removeAt(position)
+        notifyDataSetChanged()
+    }
+
 
     override fun getItemCount(): Int = items.size
 }
--- app/src/main/java/com/example/alne/view/FridgeAllFragment.kt
+++ app/src/main/java/com/example/alne/view/FridgeAllFragment.kt
@@ -1,17 +1,29 @@
 package com.example.alne.view
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
+import android.view.View.OnFocusChangeListener
 import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.alne.R
 import com.example.alne.databinding.FragmentFridgeAllBinding
 import com.example.alne.model.Food
 
 class FridgeAllFragment : Fragment() {
     lateinit var binding: FragmentFridgeAllBinding
+    val items = arrayListOf<String>("냉장", "냉동")
+    lateinit var fridgeadapter: FridgeAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -30,10 +42,38 @@ class FridgeAllFragment : Fragment() {
             add(Food("오이",0,"2023-11-26까지"))
             add(Food("오이",0,"2023-11-26까지"))
         }
-        val adapter = FridgeAdapter(testData)
-        binding.fridgeAllRv.adapter = adapter
+        fridgeadapter = FridgeAdapter(testData)
+        fridgeadapter.setMyItemClickListener(object: FridgeAdapter.MyItemClickListener{
+            override fun onItemClick(food: Food) {
+                startActivity(Intent(context,FoodActivity::class.java))
+            }
+        })
+        binding.fridgeAllRv.adapter = fridgeadapter
         binding.fridgeAllRv.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL,false )
 
+        binding.fridgeAllFloatingBt.setOnClickListener {
+            getCustomDialog()
+        }
         return binding.root
+    }
+
+    private fun getCustomDialog(){
+        val myAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, items)
+        val layoutInflater = LayoutInflater.from(context)
+        val view = layoutInflater.inflate(R.layout.item_foodadd,  null)
+        val spinner = view.rootView.findViewById<Spinner>(R.id.spinner)
+        spinner.adapter = myAdapter
+        spinner.setSelection(0)
+        val alertDialog = AlertDialog.Builder(context, R.style.Dialog_food)
+            .setView(view)
+            .create()
+        view.rootView.findViewById<AppCompatButton>(R.id.cancel_bt).setOnClickListener {
+            alertDialog.dismiss()
+        }
+        view.rootView.findViewById<AppCompatButton>(R.id.submit_bt).setOnClickListener {
+            fridgeadapter.addFood(Food("사과",0,"2023-12-20까지"))
+            alertDialog.dismiss()
+        }
+        alertDialog.show()
     }
 }
