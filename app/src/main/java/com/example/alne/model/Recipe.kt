e70f5d7d78d337bce3403d03e0264a5e41755956
yunsuk990 <yunsuk990@naver.com>
2023-12-05 01:37:11 +0900

    RecipeFragment

---
+++ app/src/main/java/com/example/alne/model/Recipe.kt
@@ -0,0 +1,12 @@
+package com.example.alne.model
+
+data class Recipe(
+    val name:String,
+    val introduce: String,
+    val category: String,
+    val classification: String,
+    val time: String,
+    val serving: String,
+    val difficulty: String,
+    val imageURL: String
+)
--- app/src/main/java/com/example/alne/view/RecipeFragment.kt
+++ app/src/main/java/com/example/alne/view/RecipeFragment.kt
@@ -10,30 +10,27 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.alne.R
 import com.example.alne.databinding.FragmentRecipeBinding
+import com.example.alne.model.Recipe
 
 class RecipeFragment : Fragment() {
     lateinit var binding: FragmentRecipeBinding
-    var check: Boolean = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentRecipeBinding.inflate(layoutInflater)
+        binding.recipeMenuAllTv.requestFocus()
 
-
-//        binding.recipeMenuAllLinear.setOnClickListener {
-//            check = !check
-//            if(check){
-//                binding.recipeMenuAllLinear.setBackgroundResource(R.drawable.image_border2)
-//            }else{
-//                binding.recipeMenuAllLinear.setBackgroundResource(R.drawable.image_border)
-//            }
-//        }
-
-
-
-
+        val items = ArrayList<Recipe>()
+        items.add(Recipe("라면","--","라면","일식","40분","40분","보통","11"))
+        items.add(Recipe("라면","--","라면","일식","40분","40분","보통","11"))
+        items.add(Recipe("라면","--","라면","일식","40분","40분","보통","11"))
+        items.add(Recipe("라면","--","라면","일식","40분","40분","보통","11"))
+        items.add(Recipe("라면","--","라면","일식","40분","40분","보통","11"))
+        items.add(Recipe("라면","--","라면","일식","40분","40분","보통","11"))
+        val gridAdapter = RecipeGVAdapter(requireContext(),items)
+        binding.recipeGv.adapter = gridAdapter
         return binding.root
     }
 }
